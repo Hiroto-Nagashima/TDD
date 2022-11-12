34dde8da05d461ce408f2ab4db5e5eb1d91c9df8
Hiroto-Nagashima <nh35813219254@gmail.com>
2022-11-12 23:50:53 +0900

    helloメソッドのテスト作成

--- hello_world_spec.rb
+++ hello_world_spec.rb
@@ -1,5 +1,10 @@
 require './modules/hello_world.rb'
 
-RSpec.describe HelloWorld, '#hello' do
-  it 'コンソールにhello worldと表示する' do
+RSpec.describe HelloWorld do
+  describe "#hello" do
+    it 'コンソールにhello worldと表示する' do
+      hello = HelloWorld.new('hiroto')
+      expect{ hello.hello }.to output('hello hiroto').to_stdout
+    end
+  end
 end
